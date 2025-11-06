a26cefce982c5130df178b070a1d449754f60196
Spring8402 <jake.austin.stephens@gmail.com>
2025-11-05 18:59:28 -0600

    messing around

---
+++ js/pages/Home.js
@@ -0,0 +1,18 @@
+// /js/pages/Home.js
+export default {
+  name: "Home",
+  template: `
+    <section class="home-section">
+      <div class="container">
+        <h1>Welcome to the Beat Saber Challenge List</h1>
+        <p>
+          The Beat Saber Challenge List (BSCL) is a community-driven ranking
+          of the toughest and most creative custom Beat Saber maps.
+        </p>
+        <p>
+          Use the navigation above to browse the leaderboard, list, or roulette challenges!
+        </p>
+      </div>
+    </section>
+  `,
+};
--- js/routes.js
+++ js/routes.js
@@ -1,8 +1,11 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import Home from "./pages/Home.js";
+
 
 export default [
+    { path: "/home", component: Home},
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
