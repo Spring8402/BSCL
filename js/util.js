a9a9e984486a748a70c9799a01b3550379d385cd
Spring8402 <jake.austin.stephens@gmail.com>
2025-11-16 22:31:46 -0600

    allow beatleader??

--- js/util.js
+++ js/util.js
@@ -1,3 +1,4 @@
+
 // https://stackoverflow.com/questions/3452546/how-do-i-get-the-youtube-video-id-from-a-url
 export function getYoutubeIdFromUrl(url) {
     return url.match(
@@ -5,32 +6,53 @@ export function getYoutubeIdFromUrl(url) {
     )?.[1] ?? '';
 }
 
-export function embed(video) {
-    return `https://www.youtube.com/embed/${getYoutubeIdFromUrl(video)}`;
+// Convert a YouTube ID or URL into an embeddable YouTube iframe URL
+export function getYoutubeEmbedUrl(url) {
+    return `https://www.youtube.com/embed/${getYoutubeIdFromUrl(url)}`;
 }
+
+// Thumbnail for YouTube videos
+export function getThumbnailFromId(id) {
+    return `https://img.youtube.com/vi/${id}/mqdefault.jpg`;
+}
+
+export function isBeatLeaderLink(url) {
+    return url.includes("replay.beatleader.com");
+}
+
+export function getBeatLeaderEmbedUrl(url) {
+    return url; 
+}
+
+
+export function embed(video) {
+    if (!video) return "";
+
+    // BeatLeader replay
+    if (isBeatLeaderLink(video)) {
+        return getBeatLeaderEmbedUrl(video);
+    }
+
+    // Otherwise treat as YouTube
+    return getYoutubeEmbedUrl(video);
+}
+
 
 export function localize(num) {
     return num.toLocaleString(undefined, { minimumFractionDigits: 3 });
 }
 
-export function getThumbnailFromId(id) {
-    return `https://img.youtube.com/vi/${id}/mqdefault.jpg`;
-}
-
+// Shuffle an array (Fisher-Yates)
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 export function shuffle(array) {
     let currentIndex = array.length, randomIndex;
 
-    // While there remain elements to shuffle.
     while (currentIndex != 0) {
-        // Pick a remaining element.
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
 
-        // And swap it with the current element.
         [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex],
-            array[currentIndex],
+            array[randomIndex], array[currentIndex],
         ];
     }
 
